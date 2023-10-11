da3a2294c66449cdd1ecabc24e15f7f2fd534b04
douglasferreirag <douglas.ferreirag@gmail.com>
2023-10-10 21:35:48 -0300

    Inicio

---
+++ Publications api/Publications-api-2023/src/main/java/me/dfg/PublicationsApi2023Application.java
@@ -0,0 +1,13 @@
+package me.dfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicationsApi2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicationsApi2023Application.class, args);
+	}
+
+}
---
+++ Publications api/Publications-api-2023/src/test/java/me/dfg/PublicationsApi2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicationsApi2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
